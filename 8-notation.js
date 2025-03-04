c67f3ab98e6a0f8412d4e72577321165b70da44e
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2025-03-05 00:26:38 +0600

    addd to a final push

---
+++ 8-notation.js
@@ -0,0 +1,15 @@
+const person = {
+    name : 'Lal Mia',
+    age: 45,
+    12: 'summer',
+    address: 'Shahabag'
+}
+ // dot notation 
+ const prof1 = person.name;
+//  console.log(prof1);
+
+ const prof2 = person['name'];
+ console.log(prof2)
+
+ const season =  person[12];
+ console.log(season);
