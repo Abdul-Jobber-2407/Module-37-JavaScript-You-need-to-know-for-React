ae46ff4e53a5cc1fa71eabdf71800d499dfd8f41
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2025-03-14 10:36:12 +0600

    add to notation

--- 6-shortcut.js
+++ 6-shortcut.js
@@ -1,9 +1,8 @@
 // 'abcd', 5, true, {}, []
 // '' , 0, false, null,undefined
 // check truthty 
-
 let myVar = 5 ;
-// check the truthy 
+// check any truthy 
 if(myVar){
     myVar = myVar*100;
 }
@@ -38,22 +37,23 @@ let drink = (money > 100 && myVar > 100) ? 'coke' : 'filter water';
 // number ot string conversion 
 const num1 = 45;
 console.log(num1);
-
 const numStr = num1 + '';
-// console.log(numStr);
+console.log(numStr);
 
 // string ot number 
 const input = '524';
 const inputNum = +input;
-// console.log(inputNum);
+console.log(inputNum);
 
 // 
 const isActive = false;
 const showUser = () => console.log('disPlay User');
 const hideUser = () => console.log('hide User');
-// isActive ? showUser(): hideUser();
+isActive ? showUser(): hideUser();
+
 // use && if the left side is true then right side will be executed 
 isActive && showUser();
+
 // use || if the left side is false then right side will be executed
 isActive || hideUser();
 
--- 7-No-Class/storage.js
+++ 7-No-Class/storage.js
@@ -1,6 +1,7 @@
 const addToLocalStorage =  () => {
     const idInput = document.getElementById('storage-id');
     const id = idInput.value ;
+    
     const valueInput = document.getElementById('storage-value');
     const value = valueInput.value;
 
